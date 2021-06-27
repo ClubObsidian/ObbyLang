539aef673cf867b22194622e14928f614ac526a5
virustotalop <virustotalop@gmail.com>
2021-06-27 05:33:28 -0700

    Start implementing cloud
    We will start testing with Bukkit and then move onto other platforms

--- bukkit/src/main/java/com/clubobsidian/obbylang/command/ObbyLangCommand.java
+++ bukkit/src/main/java/com/clubobsidian/obbylang/command/ObbyLangCommand.java
@@ -1,5 +1,10 @@
 package com.clubobsidian.obbylang.command;
 
+import cloud.commandframework.annotations.Argument;
+import cloud.commandframework.annotations.CommandMethod;
+import cloud.commandframework.annotations.CommandPermission;
+import cloud.commandframework.annotations.suggestions.Suggestions;
+import cloud.commandframework.context.CommandContext;
 import com.clubobsidian.obbylang.manager.script.ScriptManager;
 import com.clubobsidian.obbylang.pipe.Pipe;
 import com.clubobsidian.obbylang.pipe.bukkit.SenderPipe;
@@ -7,9 +12,89 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class ObbyLangCommand implements CommandExecutor {
+import java.util.List;
 
-    @Override
+public class ObbyLangCommand {
+
+    @CommandMethod("obbylang|ol")
+    @CommandPermission("obbylang.use")
+    public void base(CommandSender sender) {
+        this.sendCommandList(sender);
+    }
+
+    @CommandMethod("obbylang load <script>")
+    public void load(CommandSender sender, @Argument(value = "script", suggestions = "script") String script) {
+        Pipe pipe = new SenderPipe(sender);
+        boolean loaded = ScriptManager.get().loadScript(script, pipe);
+        if(loaded) {
+            sender.sendMessage("Script has been loaded");
+        } else {
+            sender.sendMessage("Script could not be loaded");
+        }
+    }
+
+    @CommandMethod("obbylang unload <script>")
+    public void unload(CommandSender sender, @Argument(value = "script", suggestions = "script") String script) {
+        Pipe pipe = new SenderPipe(sender);
+        boolean unloaded = ScriptManager.get().unloadScript(script, pipe);
+        if(unloaded) {
+            sender.sendMessage("Script has been unloaded");
+        } else {
+            sender.sendMessage("Script could not be unloaded");
+        }
+    }
+
+    @CommandMethod("obbylang reload <script>")
+    public void reload(CommandSender sender, @Argument(value = "script", suggestions = "script") String script) {
+        Pipe pipe = new SenderPipe(sender);
+        boolean reload = ScriptManager.get().reloadScript(script, pipe);
+        if(reload) {
+            sender.sendMessage("Script has been reloaded");
+        } else {
+            sender.sendMessage("Script could not be reloaded");
+            sender.sendMessage("Attemping to load the script");
+            boolean load = ScriptManager.get().loadScript(script, pipe);
+            if(load) {
+                sender.sendMessage("Script has been loaded");
+            } else {
+                sender.sendMessage("Script could not be loaded");
+            }
+        }
+    }
+
+    @CommandMethod("obbylang enable <script>")
+    public void enable(CommandSender sender, @Argument(value = "script", suggestions = "script") String script) {
+        Pipe pipe = new SenderPipe(sender);
+        boolean enabled = ScriptManager.get().enableScript(script, pipe);
+        if(enabled) {
+            sender.sendMessage("Script has been enabled");
+        } else {
+            sender.sendMessage("Script can not be enabled");
+        }
+    }
+
+    @CommandMethod("obbylang disable <script>")
+    public void disable(CommandSender sender, @Argument(value = "script", suggestions = "script") String script) {
+        Pipe pipe = new SenderPipe(sender);
+        boolean disabled = ScriptManager.get().disableScript(script, pipe);
+        if(disabled) {
+            sender.sendMessage("Script has been disabled");
+        } else {
+            sender.sendMessage("Script can not be disabled");
+        }
+    }
+
+    @CommandMethod("obbylang list")
+    public void list(CommandSender sender) {
+        sender.sendMessage(ScriptManager.get().getScriptListString());
+    }
+
+    @Suggestions("script")
+    public List<String> scriptSuggestions(CommandContext<CommandSender> sender, String input) {
+        return ScriptManager.get().getScriptNames();
+    }
+
+    /*@Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender.hasPermission("obbylang.use")) {
             if(args.length == 2) {
@@ -72,14 +157,14 @@ public class ObbyLangCommand implements CommandExecutor {
 
         }
         return false;
-    }
+    }*/
 
-    private void sendCommandList(String label, CommandSender sender) {
-        sender.sendMessage("/" + label + " load <script>");
-        sender.sendMessage("/" + label + " unload <script>");
-        sender.sendMessage("/" + label + " reload <script>");
-        sender.sendMessage("/" + label + " enable <script>");
-        sender.sendMessage("/" + label + " disable <script>");
-        sender.sendMessage("/" + label + " list");
+    private void sendCommandList(CommandSender sender) {
+        sender.sendMessage("/ol load <script>");
+        sender.sendMessage("/ol unload <script>");
+        sender.sendMessage("/ol reload <script>");
+        sender.sendMessage("/ol enable <script>");
+        sender.sendMessage("/ol disable <script>");
+        sender.sendMessage("/ol list");
     }
 }
--- bukkit/src/main/java/com/clubobsidian/obbylang/plugin/BukkitObbyLangPlugin.java
+++ bukkit/src/main/java/com/clubobsidian/obbylang/plugin/BukkitObbyLangPlugin.java
@@ -1,5 +1,11 @@
 package com.clubobsidian.obbylang.plugin;
 
+import cloud.commandframework.CommandManager;
+import cloud.commandframework.annotations.AnnotationParser;
+import cloud.commandframework.bukkit.CloudBukkitCapabilities;
+import cloud.commandframework.execution.CommandExecutionCoordinator;
+import cloud.commandframework.meta.SimpleCommandMeta;
+import cloud.commandframework.paper.PaperCommandManager;
 import com.clubobsidian.obbylang.ObbyLang;
 import com.clubobsidian.obbylang.ObbyLangPlatform;
 import com.clubobsidian.obbylang.command.ObbyLangCommand;
@@ -19,19 +25,29 @@ import com.clubobsidian.obbylang.manager.plugin.PluginManager;
 import com.google.inject.Injector;
 import javassist.ClassClassPath;
 import javassist.ClassPool;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.function.Function;
 
 public class BukkitObbyLangPlugin extends JavaPlugin implements ObbyLangPlugin, Listener {
 
     private static BukkitObbyLangPlugin instance;
 
     private Injector injector;
+    private CommandManager<CommandSender> commandManager;
+    private AnnotationParser<CommandSender> commandParser;
 
     @Override
     public boolean createObbyLangCommand() {
-        this.getCommand("obbylang").setExecutor(new ObbyLangCommand());
-        this.getCommand("obbylang").setTabCompleter(new ObbyLangCommandTabCompleter());
+        this.commandManager = this.createCommandManager();
+        this.commandParser = new AnnotationParser(this.commandManager,
+                CommandSender.class, parameters ->
+                SimpleCommandMeta.empty());
+        this.commandParser.parse(new ObbyLangCommand());
+        //this.getCommand("obbylang").setExecutor(new ObbyLangCommand());
+        //this.getCommand("obbylang").setTabCompleter(new ObbyLangCommandTabCompleter());
         return true;
     }
 
@@ -78,5 +94,21 @@ public class BukkitObbyLangPlugin extends JavaPlugin implements ObbyLangPlugin, 
 
     public static BukkitObbyLangPlugin get() {
         return instance;
+    }
+
+    private CommandManager<CommandSender> createCommandManager() {
+        try {
+            PaperCommandManager<CommandSender> commandManager = new PaperCommandManager(this,
+                    CommandExecutionCoordinator.simpleCoordinator(),
+                    Function.identity(),
+                    Function.identity());
+            if (commandManager.queryCapability(CloudBukkitCapabilities.BRIGADIER)) {
+                commandManager.registerBrigadier();
+            }
+            return commandManager;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
