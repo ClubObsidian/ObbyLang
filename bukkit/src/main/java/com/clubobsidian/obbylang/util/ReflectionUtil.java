602c9c44ec9b05edd916aa12ebac70e04e686e41
virustotalop <virustotalop@gmail.com>
2021-08-19 19:43:13 -0700

    Support mojang mapped servers

--- bukkit/src/main/java/com/clubobsidian/obbylang/util/ReflectionUtil.java
+++ bukkit/src/main/java/com/clubobsidian/obbylang/util/ReflectionUtil.java
@@ -5,7 +5,28 @@ public final class ReflectionUtil {
     private ReflectionUtil() {
     }
 
+    private static boolean mojangMapped = false;
+
+    static {
+        try {
+            Class.forName("net.minecraft.server.MinecraftServer");
+            mojangMapped = true;
+        } catch(ClassNotFoundException e) {
+            //Ignore
+        }
+    }
+
     public static Class<?> getMinecraftClass(String className) throws ClassNotFoundException {
+        if(mojangMapped) {
+            return Class.forName("net.minecraft." + className);
+        }
+        return Class.forName("net.minecraft.server." + VersionUtil.getVersion() + "." + className);
+    }
+
+    public static Class<?> getMinecraftClass(String packageName, String className) throws ClassNotFoundException {
+        if(mojangMapped) {
+            return Class.forName("net.minecraft." + packageName + "." + className);
+        }
         return Class.forName("net.minecraft.server." + VersionUtil.getVersion() + "." + className);
     }
 
